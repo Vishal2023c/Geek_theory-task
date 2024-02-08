d5b06fd9fef406f62e4c6812aed74fbd93bab5ca
Vishal Chouhan <146709960+Vishal2023c@users.noreply.github.com>
2024-02-08 13:17:16 +0530

    2/8/2024

---
+++ introduction_genrator.py
@@ -0,0 +1,13 @@
+print("-----------------introduce your self-------------","\n")
+print("Hello sir and Hello mem.\nThank you sir and mem for giving this chance to introduce my self.")
+Name=red.input("what is your name ?:")
+Place=input("Where are you from ?:")
+Education=input("What is your education ?:")
+Experience=input("what your experience ?:")
+Hobby=input("What is your hobby ?:")
+Strength=input("what your strength ?:")
+Goal=input("what your goal ?:")
+
+
+print("\n---------------Introduction Format Generate------------------")
+print("\n","NAME =",Name,"\n","Place =",Place,"\n","Education =",Education,"\n","Experience =",Experience,"\n","Hobby=",Hobby,"\n","Strength=",Strength,"\n","Goal=",Goal,"\n","That's all about me , Thank you.")
