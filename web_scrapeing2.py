dfecb62ee1a362277c913996e28272f3dece227e
Vishal Chouhan <146709960+Vishal2023c@users.noreply.github.com>
2024-03-08 12:33:03 +0530

    Add files via upload

--- web_scrapeing2.py
+++ web_scrapeing2.py
@@ -2,18 +2,17 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By  
 import time
 
-# username="9589335699"
-# password="H4523v95"
+username="chouhanvishal6609@gmail.com"
+password="@sgsvchouhan"
 
 driver=webdriver.Chrome()
+driver.get("https://replit.com/")
+driver.find_element(By.XPATH,"//a[@role='link']").click()
 
-driver.get("https://91club.co.in/")
-
-driver.find_element(By.CSS_SELECTOR,"input[type=\"submit\"]").click()
-driver.find_element(By.CSS_SELECTOR," input[type\"div.loginin\"]").click()
-# driver.find_element(By.CLASS_NAME,"userNumber").send_keys("9589335699")
-# driver.find_element(By.CLASS_NAME,"password").send_keys("H4523v95")
-# driver.find_element(By.CSS_SELECTOR,"button .active[data-v-ba1985c0]").click()
+driver.find_element(By.XPATH,"//a[@role='button']").click()
+driver.find_element(By.XPATH,"//input[@aria-label='Email']").send_keys(username)
+driver.find_element(By.XPATH,"//input[@aria-label='Password']").send_keys(password)
+driver.find_element(By.XPATH,"//button[@type='submit']").click()
 
 print("logging succesfully !")
-time.sleep(120)
+time.sleep(240)
